2e482caa85dd2e607b11db7cb8cdaa3ca4c84e82
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2025-05-04 22:36:28 -0400

    updating blackjack

--- main.py
+++ main.py
@@ -19,53 +19,66 @@ def calculate_score(cards):
     cards.append(1)
   return sum(cards)
 
-user_cards = [];
-computer_cards = [];
+def compare(u_score, c_score):
+  if u_score == c_score:
+    return "Draw 😐"
+  elif c_score == 0:
+    return "You lost. Dealer has Blackjack 😱"
+  elif u_score == 0:
+    return "You won with a Blackjack 😎"
+  elif u_score > 21:
+    return "You went over. You lose 😭"
+  elif c_score > 21:
+    return "Dealer went over. You win 🥳"
+  elif u_score > c_score:
+    return "You win 😃"
+  else:
+    return "You lose 🙁"
+  
 
-# for loop range operator
-for _ in range(2):
-  user_cards.append(deal_card())
-  computer_cards.append(deal_card())
+def play_blackjack():
 
-print(f"user cards: {user_cards}")
-print(f"dealer first card: {computer_cards[0]}")
+  user_cards = [];
+  computer_cards = [];
+  is_game_over = False;
+  computer_score = -1;
+  user_score = -1;
+
+  # for loop range operator
+  for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
 
 
 
-draw_card = True;
 
-while draw_card:
-
-  if calculate_score(user_cards) == 0:
-    print("You won!");
-    draw_card = False;
-  if calculate_score(computer_cards) == 0:
-    print("dealer won.");
-    draw_card = False;
-  if draw_card:
-    draw_again = input("Would you like to draw again?");
-    if calculate_score(computer_cards) <= 16:
-      computer_cards.append(deal_card());
-    if draw_again == "yes":
-      user_cards.append(deal_card());
-      print(f"user cards: {user_cards}")
-      print(f"dealer cards: {computer_cards[0]}")
+  while not is_game_over:
 
     user_score = calculate_score(user_cards);
     computer_score = calculate_score(computer_cards);
-    if user_score > 21:
-      print("You lost.")
-      draw_card = False;
-    if computer_score > 21:
-      print("You Won!")
-      draw_card = False;
-    if draw_again != "yes":
-      if(user_score > computer_score):
-        print("You Won!!")
-      else:
-        print("You lost :(")
-      draw_card = False;
-    
 
-print(f"Your score was: {calculate_score(user_cards)}")
-print(f"Dealer score was: {calculate_score(computer_cards)}")
+    print(f"Your cards: {user_cards}, current score: {user_score}")
+    print(f"dealer first card: {computer_cards[0]}")
+
+    if user_score == 0 or computer_score == 0 or user_score > 21 or user_score == 21:
+      is_game_over = True;
+    else:
+      draw_another_card = input("Type 'y' for Hit or 'n' for Stand: ")
+      if draw_another_card == "y":
+        user_cards.append(deal_card())
+      else:
+        is_game_over = True
+
+  while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
+
+  print(f"Your final hand: {user_cards}, final score: {user_score}")
+  print(f"Dealer's final hand: {computer_cards}, final score: {computer_score}")
+  print(compare(user_score, computer_score));
+
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+  print("\n" * 20)
+  play_blackjack()
+  
